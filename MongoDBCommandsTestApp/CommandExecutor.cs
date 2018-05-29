1288f12385b55d0ec9bbcaca5ca2ac8b5d7e2686
Vsevolod <seva.parshin@yandex.ru>
2018-05-30 00:02:03 +0300

    Added class for running commands

---
+++ MongoDBCommandsTestApp/CommandExecutor.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System.Configuration;
+
+namespace MongoDBCommandsTestApp
+{
+    class CommandExecutor
+    {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["MongoDb"].ConnectionString;
+
+        public static bool CompareCommandsResults (string FirstCommand, string SecondCommand)
+        {
+            return GetCommandResult(FirstCommand).Equals(GetCommandResult(SecondCommand));
+        }
+
+        public static BsonDocument GetCommandResult (string MongoCommand)
+        {
+            MongoClient client = new MongoClient(connectionString);
+            var database = client.GetDatabase("test");
+            var command = new BsonDocument(BsonSerializer.Deserialize<BsonDocument>(GetCommandParams(MongoCommand)));
+            var gotCredentials = false;
+            // users only allowed to run 'find' queryes
+            foreach (var element in command.Elements)
+            {
+                if (element.Name == "find")
+                    gotCredentials = true;
+            }
+            if (!gotCredentials)
+                throw new System.UnauthorizedAccessException();
+            return database.RunCommand<BsonDocument>(command);
+        }
+
+        private static string GetCommandParams (string MongoCommand)
+        {
+            //query must be like db.runCommand(...);
+            if (!MongoCommand.StartsWith("db.runCommand"))
+                throw new System.FormatException();
+            var partOfCommand = MongoCommand.Split('(')[1];
+            return partOfCommand.Substring(0, partOfCommand.LastIndexOf(')'));
+        }
+    }
+}
--- MongoDBCommandsTestApp/Program.cs
+++ MongoDBCommandsTestApp/Program.cs
@@ -10,18 +10,27 @@ namespace MongoDBCommandsTestApp
     {
         static void Main(string[] args)
         {
-            string connectionString = ConfigurationManager.ConnectionStrings["MongoDb"].ConnectionString;
-            MongoClient client = new MongoClient(connectionString);
-            var database = client.GetDatabase("test");
-            var commandText = Console.ReadLine();
-            // { find: 'Abonent', filter: { "Fio" : "Конюхов В.С." }}
-            // {insert: 'Abonent', documents: [{ "StreetCD" : 3.0, "HouseNo" : 1.0,"FlatNo" : 65.0,"Fio" : "Конюхов В.С.1","Phone" : "761699" }]}
-            var command = new BsonDocument(BsonSerializer.Deserialize<BsonDocument>(commandText));
-            Console.WriteLine(database.RunCommand<BsonDocument>(command).ToString());
-            do
+            try
             {
-                Console.ReadKey();
-            } while (Console.ReadKey().KeyChar != 81);
+                var str1 = Console.ReadLine();
+                var str2 = Console.ReadLine();
+                if (CommandExecutor.CompareCommandsResults(str1, str2))
+                    Console.WriteLine("Ok");
+                else
+                    Console.WriteLine("Different results");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            finally
+            {
+                int keychar = new int();
+                do
+                {
+                    keychar = Console.ReadKey().KeyChar;
+                } while (keychar != 81);
+            }
         }
     }
 }
